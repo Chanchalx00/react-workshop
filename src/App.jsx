57c6c87b956a274464d6182f6f18fa926460da38
chanchal chourasiya <chanchalchourasiya983@gmail.com>
2025-10-31 09:49:34 +0530

    add protectded routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Header } from "./components/header.jsx";
 import { Home } from "./components/Home.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProductDetails } from "./ProductDetails.jsx";
+import { LoginForm } from "./components/LoginForm.jsx";
+import { ProtectedRoute } from "./components/ProtectedRoute.jsx";
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("isLoggedIn") == "true";
+    setIsLoggedIn(token);
+  }, []);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+  const handleLogout = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/productdetails" element={<ProductDetails />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      {isLoggedIn && <Header onLogout={handleLogout} />}
+      <Routes>
+        <Route
+          path="/"
+          element={isLoggedIn ? <Home /> : <LoginForm onLogin={handleLogin} />}
+        />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute isLoggedIn={isLoggedIn}>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/productdetails"
+          element={
+            <ProtectedRoute isLoggedIn={isLoggedIn}>
+              <ProductDetails />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<LoginForm onLogin={handleLogin} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-export const Header = () => {
+import { useNavigate } from "react-router-dom";
+export const Header = ({ onLogout }) => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("isLoggedIn");
+    onLogout();
+    navigate("/login");
+  };
+
   return (
-    <header className="justify-between flex p-4 bg-gray-800 text-white">
+    <header className="justify-between flex p-4 bg-gray-800 text-white items-center ">
       <div>
         <NavLink to="/">My Store</NavLink>
       </div>
@@ -10,26 +18,29 @@ export const Header = () => {
         <div className="flex flex-row space-x-5">
           <NavLink
             to="/"
-            className={({ isActive }) => isActive ? "text-sky-500" : ""}
+            className={({ isActive }) => (isActive ? "text-sky-500" : "")}
           >
             Home
           </NavLink>
           <NavLink
             to="/productdetails"
-            className={({ isActive }) => isActive ? "text-sky-500" : ""}
+            className={({ isActive }) => (isActive ? "text-sky-500" : "")}
           >
             ProductDetails
           </NavLink>
-          <NavLink to="/about"
-            className={({ isActive }) => isActive ? "text-sky-500" : ""}
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "text-sky-500" : "")}
           >
             About
           </NavLink>
-          <NavLink to='/profile'
-            className={({ isActive }) => isActive ? "text-sky-500" : ""}
+          <NavLink
+            to="/profile"
+            className={({ isActive }) => (isActive ? "text-sky-500" : "")}
           >
             Profile
           </NavLink>
+          <button onClick={handleLogout}>Logout</button>
         </div>
       </nav>
     </header>
